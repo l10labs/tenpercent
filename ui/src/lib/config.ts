976bad524e68f531c80b2cf2e5f1332a53e75fda
elton-cs <govada.elton@gmail.com>
2025-02-25 11:10:04 -0700

    feat: update actions contract

--- ui/src/lib/config.ts
+++ ui/src/lib/config.ts
@@ -1,5 +1,5 @@
 export const katana_rpc_url = 'https://api.cartridge.gg/x/tenpercentfun/katana';
 export const torii_graphql_url = 'https://api.cartridge.gg/x/tenpercentfun/torii/graphql';
-export const manual_contract_address_from_sozo_inspect = '0x00ef660ffdd25c054442280efdb20505035219d8be079eff2e8baaed71f28554';
+export const manual_contract_address_from_sozo_inspect = '0x07a37d0f2a3b92fefb4a37db7b102ed7e245152ec1b3a2130e6f5d68451eb45a';
 export const TOKEN_BASE_DECIMALS = 10 ** 12;
 export const BOMB_TIME = 200;
