a72c538b1ed10c266a31f7050706e6d428559f4c
elton-cs <govada.elton@gmail.com>
2025-02-24 19:58:59 -0700

    temp: starknet js setup with frontend buttons to call contracts

--- frontend/src/lib/client/starknet.ts
+++ frontend/src/lib/client/starknet.ts
@@ -2,20 +2,23 @@ import { RpcProvider, Account, provider, Contract } from 'starknet';
 import manifest from '../../../../contracts/manifest_dev.json'
 
 const katana_provider = new RpcProvider({
-    nodeUrl: "http://localhost:5000",
+    nodeUrl: "http://localhost:5050",
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    }
 });
 
 // | Account address |  0x4184158a64a82eb982ff702e4041a49db16fa3a18229aac4ce88c832baf56e4
 // | Private key     |  0x6bf3604bcb41fed6c42bcca5436eeb65083a982ff65db0dc123f65358008b51
 // | Public key      |  0x4b076e402835913e3f6812ed28cef8b757d4643ebf2714471a387cb10f22be3
 
-const account_address = "0x0";
-const account_private_key = "0x0";
-
 const katana_account = new Account(
     katana_provider,
     "0x4184158a64a82eb982ff702e4041a49db16fa3a18229aac4ce88c832baf56e4",
     "0x6bf3604bcb41fed6c42bcca5436eeb65083a982ff65db0dc123f65358008b51",
+    "1" // chainId
 );
 
 let actions_contract_address = manifest.contracts[0].address;
@@ -27,4 +30,4 @@ let actions_contract_instance = new Contract(
     katana_provider,
 );
 
-export { katana_account, actions_contract_instance };
+export { katana_provider, katana_account, actions_contract_instance };
